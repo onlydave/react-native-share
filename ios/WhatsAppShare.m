43b6e52930ec687fbeb552b8936b659afe2e4773
Dave Bennett <dave@primedcanvasmedia.com>
2017-07-12 11:31:45 +0100

    use whatsapp image extension

--- ios/WhatsAppShare.m
+++ ios/WhatsAppShare.m
@@ -19,7 +19,9 @@ static UIDocumentInteractionController *documentInteractionController;
 
     if ([options objectForKey:@"message"] && [options objectForKey:@"message"] != [NSNull null]) {
         NSString *text = [RCTConvert NSString:options[@"message"]];
-        text = [text stringByAppendingString: [@" " stringByAppendingString: options[@"url"]] ];
+        if ([options[@"url"] rangeOfString:@"data:image\/([a-zA-Z]*);base64,([^\"]*)" options:NSRegularExpressionSearch].location == NSNotFound) {
+            text = [text stringByAppendingString: [@" " stringByAppendingString: options[@"url"]] ];
+        }
 
         if ([[UIApplication sharedApplication] canOpenURL: [NSURL URLWithString:@"whatsapp://app"]]) {
             NSLog(@"WhatsApp installed");
@@ -51,17 +53,23 @@ static UIDocumentInteractionController *documentInteractionController;
               // NSString *filePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"betshare.png"];
               // [UIImagePNGRepresentation(image) writeToFile:filePath atomically:YES];
             // } else {
-              NSString *filePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"betshare.jpg"];
+              NSString *filePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"betshare.wai"];
               // [UIImageJPEGRepresentation(image, 1.0) writeToFile:filePath atomically:YES];
               [data writeToFile:filePath atomically:YES];
             // }
 
             documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:filePath]];
-            // documentInteractionController.UTI = @"net.whatsapp.image";
-            documentInteractionController.UTI = @"public.image";
+             // documentInteractionController.UTI = @"net.whatsapp.image";
+//            documentInteractionController.UTI = @"public.image";
             documentInteractionController.delegate = self;
-
+            
+            NSLog(@"Try open menu from thing");
+            UIViewController *rootCtrl = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+            
+            [documentInteractionController presentOpenInMenuFromRect:CGRectMake(0.0, 0.0, 0.0, 0.0) inView:rootCtrl.view animated:YES];
             // [documentInteractionController presentOpenInMenuFromRect:CGRectMake(0, 0, 0, 0) inView:[[[[[UIApplication sharedApplication] delegate] window] rootViewController] view] animated:YES];
+
+
             NSLog(@"Done whatsapp image");
             successCallback(@[]);
         } else {
